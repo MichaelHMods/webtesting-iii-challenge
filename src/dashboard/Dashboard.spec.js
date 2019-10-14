0baceac1066f21048a4acf76ef15efd92e65eb77
MichaelHMods <mikeguadalupe@gmail.com>
2019-10-14 18:31:54 -0500

    snapshots done

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
 // Test away
+import React from 'react';
+import { render } from "@testing-library/react";
+
+
+
+import Dashboard from "./Dashboard";
+
+it ('renders', ( ) => {
+    const {asFragment} = render(<Dashboard text="Unlock"/> )
+    expect(asFragment()).toMatchSnapshot();
+})
